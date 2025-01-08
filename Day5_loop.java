486a6a9c51fcf692c2f63d57ff8dcb012378a958
24072417 <2311cs010016@mallareddyuniversity.ac.in>
2025-01-08 09:03:53 -0500

    session

--- Day5_loop.java
+++ Day5_loop.java
@@ -3,17 +3,19 @@ public class Day5_loop{
         System.out.println("mujju");
     
    for(int i=20; i<=35;i++){
-    System.out.println(i);
+        System.out.println(i);
     }
     for(int j=11;j<=19;j++){
         if(j%2==0){
-         System.out.println(j);
+             System.out.println(j);
        }
 
     }
     }
 }
-//Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front;
+//Given a string and a non-negative int n, we'll say that the front 
+//of the string is the first 3 chars, or whatever is there 
+//if the string is less than length 3. Return n copies of the front;
 
 // frontTimes("Chocolate", 2) → "ChoCho"
 // frontTimes("Chocolate", 3) → "ChoChoCho"
@@ -44,6 +46,6 @@ public class Day5_loop{
 //     for(int i=0;i<n;i++){
 //       s1=s1+str;
 //     }
-//     return s1;
+//    return s1;
 //   }
   
